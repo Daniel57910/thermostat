1940598694c32fd7ea7d9139fb54d9ca8b2c6e7b
Daniel Miller <danielmiller5791@gmail.com>
2018-04-19 14:46:17 +0100

    bsic jquery connected, need to refactor once api added

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,22 +1,27 @@
 
-function Thermostat() {}
-function TempController() {}
-function CheckTemp() {}
+function Thermostat() { }
+function TempController() { }
+function CheckTemp() { }
 
 Thermostat.prototype.temperature = 30;
 Thermostat.prototype.safeMode = true;
+Thermostat.prototype.energyLevel = "low";
+Thermostat.prototype.errorMessage = "Warning. Temperature cannot go <= 10";
 
-Thermostat.prototype.checkSafeMode = (function() {
+Thermostat.prototype.checkSafeMode = function () {
   if (this.temperature >= 26) {
-    this.safeMode = false;
+    this.safeMode = false; 
   }
-});
+  else {
+    this.safeMode = true;
+  }
+};
 
-Thermostat.prototype.resetTemperature = function() {
+Thermostat.prototype.resetTemperature = function () {
   this.temperature = 20;
 };
 
-Thermostat.prototype.checkPowerUsage = function() {
+Thermostat.prototype.checkPowerUsage = function () {
   if (this.temperature < 18) {
     this.energyLevel = "low";
   }
@@ -28,9 +33,15 @@ Thermostat.prototype.checkPowerUsage = function() {
   }
 };
 
+thermo = new Thermostat();
+console.log(thermo.safeMode);
+             
+console.log(thermo.safeMode);
+
 TempController.prototype.increase = function (temp) { return (temp + 2); };
 TempController.prototype.decrease = function (temp) { return (temp - 2); };
 
-CheckTemp.prototype.checkMin = function (temp) { 
-  if (temp <= 10) { throw new Error ("temperature is too low. ensure temperature > 10");}
+CheckTemp.prototype.checkMin = function (temp) {
+  if (temp <= 10) { throw new Error("temperature is too low. ensure temperature > 10"); }
 };
+
